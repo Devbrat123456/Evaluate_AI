64f28c0fde225a3a12e686211fe2ad5518593886
Mukul536 <smukul536@gmail.com>
2025-04-28 08:08:13 +0530

    s

---
+++ public/assets/utils/videoRecorder.js
@@ -0,0 +1,23 @@
+const openMediaDevices = async (constraints) => {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+}
+
+const startVideoStreaming =async()=>{
+
+try {
+          const constraints = { audio: { noiseSuppression: true, echoCancellation: true }, video: true };
+
+            
+            const stream = await openMediaDevices(constraints); // <-- ADD 'await' here
+            console.log('Got MediaStream:', stream);
+
+            const videoElement = document.querySelector('video#videoStreaming');
+            console.log(videoElement, stream);
+
+            videoElement.srcObject = stream;
+            videoElement.play();
+        } catch (error) {
+            console.error('Error accessing media devices.', error);
+        }
+ }
+ startVideoStreaming();
